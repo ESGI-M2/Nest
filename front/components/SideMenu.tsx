ef3c16543e6e4c2e5a92d3b302b1c9847551d0f3
Johan Mickaël <johan.rakotoniaina@mevia.fr>
2025-05-26 14:46:05 +0200

    Redirect on /login when user doesn't not have access

--- front/components/SideMenu.tsx
+++ front/components/SideMenu.tsx
@@ -3,29 +3,15 @@
 import {
     Sidebar,
     SidebarContent,
-    SidebarFooter,
-    SidebarGroup,
-    SidebarGroupContent,
-    SidebarGroupLabel,
     SidebarHeader,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import { useRouter } from "next/navigation";
-import { Button } from "./ui/button";
 import UserList from "./ui/users/user-list";
 import ConversationList from "./ui/conversations/conversation-list";
 import NewConversation from "./ui/conversations/new-conversation";
 
 export default function SideMenu() {
     const router = useRouter();
-
-    const handleLogout = () => {
-        window.localStorage.removeItem("token");
-        window.localStorage.removeItem("user");
-        router.push("/login");
-    };
 
     return (
         <Sidebar>
